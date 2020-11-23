c94171e487092beeedeebec045f644b4cef859ac
xaarrie <xaarrie@gmail.com>
2020-11-23 15:12:48 +0100

    Optimizacion proyectos

--- ShoppingUnitTest/ShopingTest/ToolTest/ShoppingCart/General/CuShoppingCartUnitTest.cs
+++ ShoppingUnitTest/ShopingTest/ToolTest/ShoppingCart/General/CuShoppingCartUnitTest.cs
@@ -6,34 +6,7 @@ using SeleniumGeneratedClasses.ToolTest.ShoppingCart.Services.General;
 using OpenQA.Selenium;
 namespace ShopingTest.ToolTest.ShoppingCart.General
 {
-    public sealed class SeleniumConfig
-    {
-        public static SeleniumConfig Current = new SeleniumConfig();
-        public string driverpath = $"U:\\Selenium\\";
-        public string rpsurl =  "http://localhost/RPSNextShopping/";
-        public string user = "admin";
-        public string password = "admin";
-        public string company = "DEMOS";
-        public SeleniumConfig()
-        {
-            SeleniumHelper.SeleniumFactoryConfig.ChromeDriverPath = driverpath;
-            RPSEnvironment.RPSBaseURL = rpsurl;
-            RPSEnvironment.DefaultUser = user;
-            RPSEnvironment.DefaultPassword = password;
-            RPSEnvironment.DefaultCodCompany = company;
-        }
-        ~SeleniumConfig()
-        {
-            Dispose();
-        }
-
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-
-            // Run at end
-        }        
-    }
+    
     public class CuShoppingCartUnitTest_Delete
     {   
          [Fact]
